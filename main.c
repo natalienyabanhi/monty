1c1862bd5071f13193c85935f508da209c46787a
natalie nyabanhi <nyabanhi@gmail.com>
2023-08-24 09:58:20 +0000

    Updated

--- main.c
+++ main.c
@@ -3,23 +3,23 @@
 #include <stdio.h>
 
 instruction_t instructions[] = {
-    {"push", push},
-    {"mod", mod},
-    {"pop", pop},
-    {"swap", swap},
-    {"add", add},
-    {"nop", nop},
-    {"sub", sub},
-    {"pall", pall},
-    {"mul", mul},
-    {"mod", mod},
-    {"rotl", rotl},
-    {"pstr", pstr},
-    {"div_op", div_op},
-    {"pchar", pchar},
-    {"pint", pint},
-    {"mul", mul},
-    {NULL, NULL}
+	{"push", push},
+	{"mod", mod},
+	{"pop", pop},
+	{"swap", swap},
+	{"add", add},
+	{"nop", nop},
+	{"sub", sub},
+	{"pall", pall},
+	{"mul", mul},
+	{"mod", mod},
+	{"rotl", rotl},
+	{"pstr", pstr},
+	{"div_op", div_op},
+	{"pchar", pchar},
+	{"pint", pint},
+	{"mul", mul},
+	{NULL, NULL}
 };
 
 /**
@@ -30,72 +30,72 @@ instruction_t instructions[] = {
  */
 int main(int argc, char *argv[])
 {
-    FILE *file;
-    stack_t *stack = NULL;
-    char line[MAX_LINE_LENGTH];
-    unsigned int line_number = 0;
-    char *opcode;
-    int found;
-    instruction_t *instr;
+	FILE *file;
+	stack_t *stack = NULL;
+	char line[MAX_LINE_LENGTH];
+	unsigned int line_number = 0;
+	char *opcode;
+	int found;
+	instruction_t *instr;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        return 1;
-    }
+	if (argc != 2)
+	{
+	fprintf(stderr, "USAGE: monty file\n");
+	return 1;
+	}
 
-    file = fopen(argv[1], "r");
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        return 1;
-    }
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+	fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+	return 1;
+	}
 
-    while (fgets(line, sizeof(line), file) != NULL)
-    {
-        line_number++;
-        opcode = strtok(line, " \t\n");
+	while (fgets(line, sizeof(line), file) != NULL)
+	{
+	line_number++;
+	opcode = strtok(line, " \t\n");
+	
+	if (opcode == NULL || opcode[0] == '#')
+        continue; /* Skip empty lines and comments */
 
-        if (opcode == NULL || opcode[0] == '#')
-            continue; /* Skip empty lines and comments */
+	if (strcmp(opcode, "queue") == 0)
+	{
+	g_mode = MODE_QUEUE; 
+	continue;
+	}
+	else if (strcmp(opcode, "stack") == 0)
+	{
+	g_mode = MODE_STACK; 
+	continue;
+	}
 
-        if (strcmp(opcode, "queue") == 0)
-        {
-            g_mode = MODE_QUEUE; 
-            continue;
-        }
-        else if (strcmp(opcode, "stack") == 0)
-        {
-            g_mode = MODE_STACK; 
-            continue;
-        }
+	found = 0;
 
-        found = 0;
+	for (instr = instructions; instr->opcode != NULL; instr++)
+	{
+	if (strcmp(opcode, instr->opcode) == 0)
+	{
+	instr->f(&stack, line_number);
+	found = 1;
+	break;
+	}
+	}
 
-        for (instr = instructions; instr->opcode != NULL; instr++)
-        {
-            if (strcmp(opcode, instr->opcode) == 0)
-            {
-                instr->f(&stack, line_number);
-                found = 1;
-                break;
-            }
-        }
-
-        if (!found)
-        {
-            fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-            fclose(file);
-            /* Free the stack nodes */
+	if (!found)
+	{
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+	fclose(file);
+	/* Free the stack nodes */
             /* ... */
-            return 1;
-        }
-    }
+	return 1;
+	}
+	}
 
-    fclose(file);
-    /* Free the stack nodes */
+	fclose(file);
+	/* Free the stack nodes */
     /* ... */
 
-    return 0;
+	return 0;
 }
 
--- pint.c
+++ pint.c
@@ -7,24 +7,24 @@
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL)
-    {
-        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    printf("%d\n", (*stack)->n);
+	if (*stack == NULL)
+	{
+	fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+	exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
 }
 void free_all(stack_t **stack)
 {
-    stack_t *current = *stack;
-    stack_t *temp;
+	stack_t *current = *stack;
+	stack_t *temp;
 
-    while (current != NULL)
-    {
-        temp = current;
-        current = current->next;
-        free(temp);
-    }
+	while (current != NULL)
+	{
+	temp = current;
+	current = current->next;
+	free(temp);
+	}
 }
 
 
